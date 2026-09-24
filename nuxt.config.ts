b43977401bb7ccb35cad53dc3b2e4d7d3d5a9c80
saint sinner <alexlloyd221999@gmail.com>
2026-09-24 18:55:33 +0300

    4rd try

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -44,6 +44,8 @@ export default defineNuxtConfig({
   },
 
   app: {
+    baseURL: '/dom-project/', 
+    buildAssetsDir: 'assets', 
     head: {
       charset: "utf-8",
       viewport: "width=device-width, initial-scale=1",
